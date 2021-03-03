ba11e792919ba533e7944b2085328f033db8f0c3
junyakonishi <jmmk318@gmail.com>
2021-03-03 12:10:06 +0900

    komi

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_03_02_103802) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -43,9 +42,6 @@ ActiveRecord::Schema.define(version: 2021_03_02_103802) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
-ActiveRecord::Schema.define(version: 2021_03_02_085159) do
-
 
   create_table "tsukasa_ms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
