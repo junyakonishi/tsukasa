42c610997a26c90e7e1bba03e674d50a7b25ffb0
Nulsea <wm7mw7@gmail.com>
2021-03-05 14:13:09 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,6 +11,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    # binding.pry
     if @item.valid?
       @item.save
       redirect_to root_path
